66c67a802c590735700721b09b9f4454a131b2f2
Brian <wamwangimathenge@gmail.com>
2023-04-15 15:32:21 +0300

    Sessions controller post /login route

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+    before_action :administration
+    skip_before_action :administration, only: [:create, :destroy, :in]
+    skip_before_action :authorize, only:[:create, :in, :out]
+
+    # POST /login
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            sesssion[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+        end
+end
