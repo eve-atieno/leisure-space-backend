f1752252ad8de2a7c21e412148c917d7793ae1e0
eve-atieno <atienoevelyne53@gmail.com>
2023-04-15 23:48:02 +0300

    added routes

--- db/migrate/20230414121921_create_admins.rb
+++ db/migrate/20230414121921_create_admins.rb
@@ -3,7 +3,8 @@ class CreateAdmins < ActiveRecord::Migration[7.0]
     create_table :admins do |t|
       t.string :name
       t.string :email
-      t.string :password
+      t.string :password_digest
+      
 
       t.timestamps
     end
