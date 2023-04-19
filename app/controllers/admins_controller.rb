265cd8bddb1263148f94deb30b8730828ec9aca6
Brian <wamwangimathenge@gmail.com>
2023-04-19 10:08:16 +0300

    Admin route functions modified

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,8 +1,11 @@
 class AdminsController < ApplicationController
+  before_action :administration, only: [:show]
+  skip_before_action :authorize, only: [:show, :create]
 	
+  # GET /admin
   def show
     admin = Admin.find(session[:admin_id])
-    render json: admin
+    render json: admin, status: :created
   end
 
 end
