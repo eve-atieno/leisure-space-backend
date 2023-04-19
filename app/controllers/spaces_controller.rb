7b72915d3031272a996c8444c15f6f2c8d936b44
Brian <wamwangimathenge@gmail.com>
2023-04-19 12:06:00 +0300

    Spaces controller modified routes and syntax

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,37 +1,54 @@
 class SpacesController < ApplicationController
 
-	before_action :authorize
-	skip_before_action :authorize, only: %i[index show]
+	before_action :find_space, only: %i[ show update destroy ]
+	skip_before_action :authorize, only: %i[ index show create update destroy ]
 	
+	# GET /items
 	def index
-		spaces = Space.all
-		render json: spaces, status: :ok
+		@spaces = Space.all
+		render json: @spaces, status: :ok
 	end
 
+	# GET /items/:id
 	def show
-		space = find_space
-		render json: space, status: :ok
+		render json: find_space, status: :ok
 	end
 
+	# POST /spaces
 	def create
 		space = Space.create!(space_params)
-
 		render json: space, status: :created
+	end
+
+	# PATCH /items/:id
+	def update
+		space = find_space
+		space.update(find_space)
+		render json: space, status: :created
+	end
+
+	# DELETE /items/:id
+	def destroy
+		find_space.destroy
 	end
 
 
 	private
 
 	def find_space
-		Space.find(params[:id])
+		@space = Space.find_by(id: params[:id])
 	end
 
 	def space_params
-		params.permit(:name, :description, :image_url)
+		params.require(:space).permit(:name, :price, :location, :description)
 	end
 
-	def authorize
-		render json: {error: "Not authorized to perform this action"}, status: :unauthorized unless session.inlude? :admin_id
+	# def authorize
+	# 	render json: {error: "Not authorized to perform this action"}, status: :unauthorized unless session.inlude? :admin_id
+	# end
+
+	def render_not_found_response
+		render json: { error: "Space not found" }, status: :not_found
 	end
 end
 
