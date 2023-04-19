d2d285edc20f29d0aa497e828d6fe71340bc195c
Brian <wamwangimathenge@gmail.com>
2023-04-19 12:31:24 +0300

    Categories routes created ans spaces routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :galleries
-  resources :names
+  # resources :names
   resources :reviews
   resources :bookings
   resources :admins
   resources :users
-  resources :spaces, only: [:index, :show, :create]
+  resources :spaces, only: [:index, :show, :create, :update, :destroy]
+  resources :categories, only: [:index, :show, :destroy, :create]
 
   get "/login", to: "user#login"
 
