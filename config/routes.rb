00d802a481446e9b88d916c1b0d0e5c1c44826ff
Brian <wamwangimathenge@gmail.com>
2023-04-19 14:15:13 +0300

    Route syntax modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
-  resources :galleries
+  resources :galleries, only: [:show, :index, :create]
   # resources :names
-  resources :reviews
-  resources :bookings
-  resources :admins
-  resources :users
+  resources :reviews, only: [:show, :index, :create]
+  resources :bookings, only: [:show, :index, :create, :destroy, :checkout]
+  # resources :admins, only: [:show, :create]
+  # resources :users, only: [:show, :create]
   resources :spaces, only: [:index, :show, :create, :update, :destroy]
-  resources :categories, only: [:index, :show, :destroy, :create]
+  resources :categories, only: [:index, :show, :destroy, :create, :update]
 
-  get "/login", to: "user#login"
+  post "/signup", to: "users#create"
+  post "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  # admin
+  post "/adminin", to: "sessions#in"
+  delete "/adminout", to: "sessions#out"
+  get "/ad", to: "admins#show"
+  post "/newadmin", to: "admins#create"
+
 
 end
