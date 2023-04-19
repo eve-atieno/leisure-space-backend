bd29306095d5440657f0de73fd6e42846ae6d277
Brian <wamwangimathenge@gmail.com>
2023-04-19 10:13:34 +0300

    Admin authorization

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -8,4 +8,21 @@ class AdminsController < ApplicationController
     render json: admin, status: :created
   end
 
+  # POST /newadmin
+  def create
+    admin = Admin.create!(admin_params)
+    render json: admin, status: :created
+  end
+
+
+  private
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+  def admin_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+
 end
