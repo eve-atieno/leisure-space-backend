1a100018e665bd6d9a5b940c95bc9d4809221951
Brian <wamwangimathenge@gmail.com>
2023-04-21 13:43:58 +0300

    Updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
    has_secure_password
     has_one :profile
 
-    validates :email, presence: true, uniqueness: true
+    # validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: { minimum: 6 }
     
 end
