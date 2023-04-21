95340055f379695d73f80d13e94c170ad88634a0
Brian <wamwangimathenge@gmail.com>
2023-04-21 12:24:32 +0300

    Users controller modified

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,47 @@
 class UsersController < ApplicationController
 
-skip_before_action :authorize, only:[:create]
+    skip_before_action :authorize, only:[:create]
 
-# def index
-# 	users = User.all
-# 	render json: users
-# end
+    # def index
+    # 	users = User.all
+    # 	render json: users
+    # end
 
-# GET /me
-def show
-	user = User.find_by(id: session[:user_id])
-	render json: user, status: :created
-end
+    # GET /me
+    def show
+        user = User.find_by(id: session[:user_id])
+        render json: user, status: :created
+    end
 
-# POST /signup
-def create
-	user = User.create!(user_params)
-	session[:user_id] = user.id
-	render json: user, status: :created
-end
+    # POST /signup
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
 
-# def update
-# 	user = find_user
-# 	user.update!(user_params)
+    # def update
+    # 	user = find_user
+    # 	user.update!(user_params)
 
-# 	render json: user, status: :created
-# end
+    # 	render json: user, status: :created
+    # end
 
-private
+    private
 
-# def authorize
-# 		return render json: {error: "Not authorized to perform this action"}, status: :unauthorized unless session.inlude? :admin_id
-# end
+    # def authorize
+    # 		return render json: {error: "Not authorized to perform this action"}, status: :unauthorized unless session.inlude? :admin_id
+    # end
 
-# def find_users
-# 	User.find(params[:id])
-# end
+    # def find_users
+    # 	User.find(params[:id])
+    # end
 
-def set_user
-	@user = User.find(params[:id])
-end
+    def set_user
+        @user = User.find(params[:id])
+    end
 
-def user_params
-	params.permit(:username, :password, :password_confirmation)
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
 end
